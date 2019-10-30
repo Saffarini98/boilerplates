df6450e6f61385d36f5f4cdbc89e0fc02d34c76f
Maxim <maximsaffarini@gmail.com>
2019-10-31 00:43:42 +0100

    Changes telated to sass-middleware

--- node-template/app.js
+++ node-template/app.js
@@ -1,6 +1,8 @@
+const path = require("path");
+const sassMiddleware = require("node-sass-middleware");
+
 var express = require('express');
 var exphbs  = require('express-handlebars');
-
 var app = express();
 
 app.engine('handlebars', exphbs());
@@ -13,5 +15,17 @@ app.get('/', function (req, res) {
 app.get('/about', function (req, res) {
     res.render('about');
 });
+
+app.use(
+  sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, "scss"),
+    dest: path.join(__dirname, "public/css/"),
+    // debug: true,
+    outputStyle: "compressed",
+    prefix: "/css"
+  })
+);
+
 app.use(express.static("public"));
 app.listen(3000);
